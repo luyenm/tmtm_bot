cdc58833c8be9c60b765259f86388b2c8a0928d4
Arlios <luyen_mai@Live.com>
2018-12-14 04:09:58 -0800

    Basic chat filter implemented

---
+++ Administrative/channel_filter.py
@@ -0,0 +1,25 @@
+import re
+import pandas as pd
+import numpy as np
+
+data = pd.read_csv('Administrative/dictionary.csv')
+blacklist = data.loc[:, 'BlackList'].dropna()
+whitelist = data.loc[:, 'WhiteList'].dropna()
+
+
+# Sifts through the black and whitelist for words in the messages
+# If a word is in the white-list it ignores the word.
+# Returns a boolean depending if it finds a word in the blacklist
+def checkphrase(message):
+    phrase = message.content.lower()
+    processed_word = ''
+    if any(safe_words in phrase for safe_words in whitelist.tolist()):
+        for safe_word in whitelist.tolist():
+            processed_word = phrase[:phrase.index(safe_word)] + phrase[len(safe_word) * 2:]
+    else:
+        processed_word = phrase
+    print(processed_word)
+    if any(banned_words in processed_word.replace(' ', '') for banned_words in blacklist.tolist()):
+        return 1
+    return 0
+
